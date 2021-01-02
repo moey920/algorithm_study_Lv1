3d0c1fcdb84ac2ba289f777b51dd0f0d5e168357
하람 노 <moey920@naver.com>
2021-01-02 08:39:16 +0000

    Update basic_test/무어의 법칙[Moore&#39;s Law].py

--- basic_test/무어의 법칙[Moore's Law].py
+++ basic_test/무어의 법칙[Moore's Law].py
@@ -3,6 +3,6 @@ result = 2**N
 result = str(result) #16384를 각 자리수의 값으로['1','6','3','8','4']로 바꾸기 위해 문자열로 형변환을 해주었다.
 count = 0
 for i in range(len(result)) :
-    count += int(result[i]) #['1','6','3','8','4']를 각각 더하기 위해 숫자형으로 형변환을 해주었다.
+    count += int(result[i]) #['1','6','3','8','4']를 각각 더하기 위해(1+6+3+8+4) 숫자형으로 형변환을 해주었다.
 
 print(count)
