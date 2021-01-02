6ad8b4fd23946863a3212f98e21672433ed5bebe
하람 노 <moey920@naver.com>
2021-01-02 06:59:38 +0000

    Update basic_test/도도새 주방장.py

--- basic_test/도도새 주방장.py
+++ basic_test/도도새 주방장.py
@@ -1,12 +1,14 @@
 dodo = list(map(int,input().split()))
 cook_time = list(map(int,input().split()))
 sum_cook = 0
+count = 0
 
-for i in range(len(cook_time)) :
-    sum_cook += cook_time[i]
-    if sum_cook > dodo[1] :
-        sum_cook -= cook_time[i]
-        print(i)
-        break
         
-# print(sum_cook)
+for i in range(len(cook_time)) :
+    if(sum_cook + cook_time[i] > dodo[1]) :
+        break
+    else :
+        count += 1
+        sum_cook += cook_time[i]
+
+print(count)
